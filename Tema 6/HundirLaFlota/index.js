4df8172242fe0b85bdf4ee80ae269fb3597666f0
MariusTNTX <molinamario.msc@gmail.com>
2023-01-30 16:55:46 +0100

    ?

--- Tema 6/HundirLaFlota/index.js
+++ Tema 6/HundirLaFlota/index.js
@@ -1,21 +1,22 @@
 let cuadricula = document.getElementById("cuadricula"),
-fila, col, div, span, array=[], 
+fila, col, div, span, cuadric=[], 
 letras = ['A','B','C','D','E','F','G','H','I','J'], 
 barcos=['xl','lg','lg','md','md','md','sm','sm','sm','sm']; 
 
 fila = document.createElement("tr");
 fila.appendChild(document.createElement("td"));
 
+//Fill del cuadric y primera fila
 for(let i=0; i<10; i++){
-  array[i] = [];
-  for(let j=0; j<10; j++) array[i][j] = 0;
+  cuadric[i] = [];
+  for(let j=0; j<10; j++) cuadric[i][j] = 0;
   col = document.createElement("td");
   col.textContent = letras[i];
   fila.appendChild(col);
 }
-
 cuadricula.appendChild(fila);
 
+//Instancia del Cuadric
 for(let i=0; i<10; i++){
   fila = document.createElement("tr");
   for(let j=0; j<11; j++){
@@ -23,15 +24,17 @@ for(let i=0; i<10; i++){
     if(j==0) col.textContent=i+1;
     else{ 
       col.setAttribute("id",letras[i]+'-'+j);
-      array[i][j] = col;
+      cuadric[i][j] = col;
     }
     fila.appendChild(col);
   }
   cuadricula.appendChild(fila);
 }
 
+
 for(barco of barcos){
   span = document.createElement("span");
+  span.setAttribute("draggable","true");
   span.classList.add("barco");
   span.classList.add(barco);
   document.getElementById("barcos").appendChild(span);
